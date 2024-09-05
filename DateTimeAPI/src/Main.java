99763b60cad4ccc8bfe5d1120248bda7ba46232a
gbuxss <jeevandhamala095@gmail.com>
2024-09-05 10:10:58 -0500

    Add dateTime Java8 API

---
+++ DateTimeAPI/src/Main.java
@@ -0,0 +1,9 @@
+import java.time.ZonedDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
